787fd779bcd1242a7dd14dcbd40b65a2767b98d4
Adam <alane001@ucr.edu>
2015-04-11 16:58:47 -0700

    fixed array problem and added simpsons rule

--- Week2/Ex1.py
+++ Week2/Ex1.py
@@ -1,14 +1,12 @@
 import numpy as np
 
 def trapez(x, y, N, f):
-	
-	#Are we supposed to be able to input a function, f, and N? or are we supposed to just have the two arrays (x,y) input?
-
+	# Trapezoid equation: I = h [0.5*f(a) + 0.5*f(b) + (sum of all f(a+kh))]
 
 	# Each slice width
 	h = (y - x) / N
 	
-	#Ok I fixed the array part here above, i had float(y-x) which wouldnt allow th	e array to function
+	#Ok I fixed the array part here above, i had float(y-x) which wouldnt allow the array to function
 
 
 	# I is integral, this is first part of integral, 1/2f(a) + 1/2f(b)
@@ -17,6 +15,7 @@ def trapez(x, y, N, f):
 	# Runs through k slice of trapezoids, sum from k =1 to N, f(a+kh)
 	for k in range(1, N):
 		I += f(x+k*h)
+	
 	# Last part to to multiply by outside h
 	I = h * I
 	return I
@@ -35,4 +34,38 @@ print('This is the integral: ', q)
 
 
 
+#The simpson's rule works as well now I believe
+
+
+def simpson(x, y, N, f):
+	# The simpson equation: I = (1/3)*h [f(a) + f(b) + (4 * (sum of odd f(a+kh))) + (2 * (sum of even f(a+kh)))]
+
+	#Slice width
+	h = (y - x) / N
+
+	# 1st part of equation
+	I1 = f(x) + f(y)
+	
+	#Sum up odd numbers, 1 start, up to N, by values of 2 (1,3,5) etc
+	#keeps I1 separate so can multiply by 4
+	I2 = 0.0
+	for k in range(1, N, 2):
+		I2+= f(x+k*h)
+	I2 = I2 * 4
+	
+	#Sum up even numbers
+	I3 = 0.0
+	for j in range(2, N, 2):
+		I3 += f(x+j*h)	
+	I3 = I3 * 2
+	
+	#makes full integral
+	I = I1 + I2 + I3
+
+	# finishes up equation
+	I = (I * h) / 3.
+	return I
+
+t = simpson(0.0, 2.0, 10, lambda x: (x**4-2*x+1))
+print('Integral for simpson: ', t)
 
