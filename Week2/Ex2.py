cfb02a90da695443329271b5dc476e609ebfa10d
Adam <alane001@ucr.edu>
2015-04-12 16:27:57 -0700

    updated Ex2

--- Week2/Ex2.py
+++ Week2/Ex2.py
@@ -2,25 +2,41 @@ import Ex1 as Ex1
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Could you look at this Ex as well. Is it outputting the correct shape of graph? I wasnt sure if the equation would should be inputting is x = vt + x(initial). Since there is no gravity in the x direction. 
-
-
 #Loading given txt into an array
-a = np.loadtxt("velocities.txt", float)
+velArray = np.loadtxt("velocities.txt", float)
+
+#This creats a list of just the times
+listTime = []
+j=0
+while j < velArray.shape[0]:
+	listTime.append(velArray[j, 0])
+	j += 1
+#This changes the list into an array
+arrayTime = np.array(listTime)
+
+#This creates a list of just the velocities
+listVel=[]
+k=0
+while k < velArray.shape[0]:
+	listVel.append(velArray[k, 1])
+	k += 1
+#This changes the list into an array
+arrayVel = np.array(listVel)
 
 #empty array to start
-listVel = []
+listPos = []
 i=0
 x=0
 #loop to fill up list with x positions from velocities
-while i < a.shape[0] - 1:
-	#call trapezoidal rule from previous created program I created
-	x = Ex1.trapez(float(a[i, 0]), float(a[i+1, 0]), 10, lambda t: float(a[i, 1])*t+x)
-	listVel.append(x)
+while i < arrayTime.shape[0]:
+	#call trapezoidal rule from previous program I created
+	x = Ex1.trapez(arrayTime[i], arrayVel[i])
+	listPos.append(x)
 	i += 1
 
 #converting list to array for plotting and saving
-arrayPos = np.array(listVel)
+arrayPos = np.array(listPos)
+print(arrayPos)
 #saves as txt file
 np.savetxt('List of Positions Trapez.txt', arrayPos, newline = '\r\n')
 
@@ -31,28 +47,28 @@ plt.xlabel('Time (s)')
 plt.ylabel('Position (m)')
 plt.suptitle('Time Versus Position and Velocity')
 ax2 = plt.subplot(2,1,2)
-plt.plot(a, color = 'blue')
+plt.plot(velArray, color = 'blue')
 plt.xlabel('Time (s)')
 plt.ylabel('Velocity (m/s)')
 plt.show()
 
-# now for simposons rule
+# Now for simposons rule
 
 listVelSimp = []
-j=0
-y=0
-while j < a.shape[0] - 1:
-	#print(j)
-	y = Ex1.simpson(float(a[j, 0]), float(a[j+1, 0]), 10, lambda x: float(a[j, 1])*x+y)
-	#print(y)
-	listVelSimp.append(y)
-	j += 1
+#m=0
+#y=0
+#while m < velArray.shape[0]:
+y = Ex1.simpson(arrayTime, arrayVel)
+#	print(y)
+listVelSimp.append(y)
+#	m += 1
 
 #change to array
 arrayPosSimp = np.array(listVelSimp)
 
+print(arrayPosSimp)
 #saves output of array for position of simpsons rule
-np.savetxt('List of Positions Simp.txt', listVelSimp, newline='\r\n')
+np.savetxt('List of Positions Simp.txt', arrayPosSimp, newline='\r\n')
 
 #Graphs to compare the simpson's rule to trapezoidal rule
 
