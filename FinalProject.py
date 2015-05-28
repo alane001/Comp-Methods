0c0078fcd1ad033a25ee11b1fbae269e66e64cae
Adam <alane001@ucr.edu>
2015-05-28 00:18:05 -0700

    final project update

--- FinalProject.py
+++ FinalProject.py
@@ -12,61 +12,26 @@ M = 1.9891e30 # kg      Mass of the Sun
 # d^2 x/dt^2 = -GM x/r^3
 # d^2 y/dt^2 = -GM y/r^3           where r = sqrt(x**2 + y**2)
 
-#def fx(r, v, t):
-#	x = r[0]
-#	y = r[1]
-#	s = v[0]
-#	fx = s
-#	fs = -G * M * (x / ((x**2 + y**2)**(3/2)))
-#	return np.array([fx, fs],float)
-
-
-#def fy(r, v, t):
-#	x = r[0]
-#	y = r[1]
-#	u = v[1]
-#	fy = u
-#	fu = -G * M * (y / ((x**2 + y**2)**(3/2)))
-#	return np.array([fy, fu],float)
-
-#IF USING r1 and r2
-def fx(r,t):
-	x = r[0]
-	y = r[1]
-	v = r[2]
-	fx = v
-	fv = -G * M * (x / ((x**2 + y**2)**(3/2)))
-	return np.array([fx,fv], float)
-
-def fy(r,t):
-	x = r[0]
-	y = r[1]
-	v = r[2]
-	fy = v
-	fv = -G * M * (y / ((x**2 + y**2)**(3/2)))
-	return np.array([fy,fv],float)
-
-
-
 def f(r):
 	x = r[0]
 	vx = r[1]
 	y = r[2]
 	vy = r[3]
+	r = sqrt(x**2 + y**2)
 	fx = vx
 	fy = vy
-	#fv = -G * M * (sqrt((x-4e9)**2 + (y - 0)**2) / ((x**2 + y**2)**(3/2)))
-	fvx = -G * M * (x / ((x**2 + y**2)**(3/2)))
-	fvy = -G * M * (y / ((x**2 + y**2)**(3/2)))
-	print(fvx, fvy)
-	return np.array([fvx,fx,fvy,fy],float)
+	fvx = -G * M * (x / r**3) 
+	fvy = -G * M * (y / r**3) 
+	#print(fvx, fvy)
+	return np.array([fx,fvx,fy,fvy],float)
 
-#This is time
+#Time, dont use too high of a value for b or else causes odd orbit paths
 a = 0.0 #intial time
-b = 1e9 #final time
+b = 3e10 #final time
 
-#How many bins/steps
-N = 10
+#Make sure to have N large enough, without it causes wierd things to happen
+#How many bins/steps, the more steps the smoother the curve in the graph
+N = 1000000
 
 #Step size
 h = (b-a)/N
@@ -75,29 +40,28 @@ tpoints = np.arange(a,b,h)
 xpoints = np.zeros(N)
 ypoints = np.zeros(N)
 
-# Initial Conditions   [x , y]
-#ro = np.array([4e9,0], float)   #initial postions in km   MIGHT HAVE TO CHANGE
-#vo = np.array([0,500],float)    #initial velocities in m/s
-
-#OR can change r into r(xo, yo, vo) where it is a function of position and velocity
-#r1 = np.array([4e9, 0, 0],float)
-#r2 = np.array([4e9, 0, 500],float)
+# Initial Conditions
 
 # r = (x,s,y,u) where s is velocity x and u is velocity y
 # Converted to m from km
 r = np.array([4e12, 0, 0, 500],float)
 
 # Fourth order Runge-Kutta Method for ODE's
-for i in range(N): 
-    t = tpoints[i]
-    xpoints[i] = r[0]
-    ypoints[i] = r[2]
-    k1 = h*f(r)#)
-    k2 = h*f(r+0.5*k1)#,t+0.5*h)
-    k3 = h*f(r+0.5*k2)#,t+0.5*h)
-    k4 = h*f(r+k3)#,t+h)
-    r += (k1+2*k2+2*k3+k4)/6
-
-print(np.min(xpoints), np.max(xpoints), np.min(ypoints), np.max(ypoints))
+for i in range(N):
+	t = tpoints[i]
+	xpoints[i] = r[0]
+	ypoints[i] = r[2]
+	k1 = h*f(r)
+	k2 = h*f(r+0.5*k1) 
+	k3 = h*f(r+0.5*k2)
+	k4 = h*f(r+k3)
+	r += (k1+2*k2+2*k3+k4)/6
+	
+#print(xpoints)
+#print(np.min(xpoints), np.max(xpoints), np.min(ypoints), np.max(ypoints))
 plt.plot(xpoints, ypoints)
-plt.show()
+plt.xlabel("X Position (m)")
+plt.ylabel("Y Position (m)")
+plt.title("Position of the Comet")
+plt.savefig("Position of the Comet.png", format = "png")
+#plt.show()
